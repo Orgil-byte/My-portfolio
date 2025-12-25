819db9d06d2f20ee63d64ce4d833c6761b8f6cf3
U.Orgil <ulziitogtohorgil1908@gmail.com>
2025-12-25 13:00:59 +0800

    A

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const hamburgerBtn1 = document.querySelector(`.stick1`);
-const hamburgerBtn2 = document.querySelector(`.stick2`);
-const hamburgerBtn3 = document.querySelector(`.stick3`);
-const hamburgerBtn = document.querySelector(`.hamburger-menu`);
-const mainNavPhone = document.querySelector(`.phone-main-nav`);
-const heroLabel = document.querySelector(`.hero-label`);
+const hamburgerBtn1 = document.querySelector(".stick1");
+const hamburgerBtn2 = document.querySelector(".stick2");
+const hamburgerBtn3 = document.querySelector(".stick3");
+const hamburgerBtn = document.querySelector(".hamburger-menu");
+const mainNavPhone = document.querySelector(".phone-main-nav");
+const heroLabel = document.querySelector(".hero-label");
 
 const navigationBurger = () => {
-  mainNavPhone.classList.toggle(`left`);
-  hamburgerBtn1.classList.toggle(`rotate45`);
-  hamburgerBtn3.classList.toggle(`rotate-45`);
-  hamburgerBtn2.classList.toggle(`display-none`);
-  heroLabel.classList.toggle(`display-none`);
+  mainNavPhone.classList.toggle("left");
+  hamburgerBtn1.classList.toggle("rotate45");
+  hamburgerBtn3.classList.toggle("rotate-45");
+  hamburgerBtn2.classList.toggle("display-none");
+  heroLabel.classList.toggle("display-none");
 };
 
-hamburgerBtn.addEventListener(`click`, navigationBurger);
+hamburgerBtn.addEventListener("click", navigationBurger);
