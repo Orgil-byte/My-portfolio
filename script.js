fe5766b635fb0faa9f4b3535021a200d9acd3a9e
U.Orgil <ulziitogtohorgil1908@gmail.com>
2025-12-25 12:46:54 +0800

    A

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+const hamburgerBtn1 = document.querySelector(`.stick1`);
+const hamburgerBtn2 = document.querySelector(`.stick2`);
+const hamburgerBtn3 = document.querySelector(`.stick3`);
 const hamburgerBtn = document.querySelector(`.hamburger-menu`);
 const mainNavPhone = document.querySelector(`.phone-main-nav`);
 const heroLabel = document.querySelector(`.hero-label`);
 
 const navigationBurger = () => {
   mainNavPhone.classList.toggle(`left`);
-  hamburgerBtn.classList.toggle(`rotate90`);
+  hamburgerBtn1.classList.toggle(`rotate45`);
+  hamburgerBtn3.classList.toggle(`rotate-45`);
+  hamburgerBtn2.classList.toggle(`display-none`);
   heroLabel.classList.toggle(`display-none`);
 };
 
